9f965fb6b80557a0aee4dd3c6c925e2df88deff1
三好大悟 <miyoshidaigo@Daigo-MacBookPro.local>
2017-05-06 20:11:39 +0900

    solved problem2

--- src/project_euler.py
+++ src/project_euler.py
@@ -5,3 +5,24 @@ def problem1(stop):
             sum += x
     return sum
 
+
+def problem2(stop):
+    """
+    written by miyoshi^^
+    date 2017/05/06
+    """
+
+    a, b = 1, 2
+    numlist = [a,b]
+    while True:
+        numlist.append(a+b)
+        a, b = b, a+b
+        if numlist[-1] > stop:
+            numlist.pop()
+            break
+
+    sum = 0
+    for x in numlist:
+        if x % 2 == 0:
+            sum += x
+    return(sum)
