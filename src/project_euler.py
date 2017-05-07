64c5bae7d27b95701730be2356a7b3e65d934299
yutaka2487 <yutaka2487@gmail.com>
2017-05-08 03:27:49 +0900

    solved problem40

--- src/project_euler.py
+++ src/project_euler.py
@@ -145,3 +145,23 @@ def problem30(exp):
     else:
         return sum(cand)
 
+def problem40():
+    """
+    https://projecteuler.net/problem=40
+    
+    An irrational decimal fraction is created by concatenating the positive integers:
+    0.123456789101112131415161718192021...
+    It can be seen that the 12th digit of the fractional part is 1.
+    If dn represents the nth digit of the fractional part, find the value of the following expression.
+    d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+    
+    >>> problem40()
+    210
+    
+    """
+    s = "".join(str(n) for n in range(1000000))
+    result = 1
+    for exp in range(7):
+        result *= int(s[10**exp])
+    else:
+        return result
