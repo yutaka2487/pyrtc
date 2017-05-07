edad369b02d0a5a3f6c4d77c85f5d6e314f83c96
yutaka2487 <yutaka2487@gmail.com>
2017-05-08 01:59:55 +0900

    solved problem5

--- src/project_euler.py
+++ src/project_euler.py
@@ -49,3 +49,27 @@ def problem4(digit):
                 cand.append(z)
     return max(cand)
 
+def problem5(to):
+    """
+    https://projecteuler.net/problem=5
+    
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    
+    >>> problem5(10)
+    2520
+    
+    >>> problem5(20)
+    232792560
+    
+    """
+    from sympy import factorint
+    factor = {}
+    for x in range(1, to+1):
+        for base, exp in factorint(x).items():
+            factor[base] = max(exp, factor.get(base, 0))
+    else:
+        result = 1
+        for base, exp in factor.items():
+            result *= base ** exp
+        return result
